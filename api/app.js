16a295da9d46636ef05ba93692f2f92169999c2f
deepakmeena78 <deepakmeenaa78@gmail.com>
2025-07-06 21:13:53 +0530

    app.js changes

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ connectDB();
 
 import cors from "cors";
 app.use(cors({
-  origin:'http://localhost:3000'
+  origin:['http://localhost:3000','https://easy-solution-frontend.vercel.app']
 }));
 
 app.use(express.json());
